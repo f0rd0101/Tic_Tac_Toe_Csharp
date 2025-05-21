011ca1454267991d57b79c9abe73db465263092c
Nick <melancholic8078@gmail.com>
2025-05-21 12:50:27 +0300

    Added Players and WinCheck

--- Program.cs
+++ Program.cs
@@ -3,6 +3,31 @@ using System.Globalization;
 
 class ConsoleApp1
 {
+    // Класс Игроков, нужен для того чтобы игроки впринципе существовали как некая сущность
+    class Player
+    {
+        private string name;
+        private char symbol;
+          
+        //Конструктор класса, который включает обязательные параметры при создании объекта класса
+        public Player(string name, char symbol)
+        {
+            this.name = name;
+            this.symbol = symbol;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public char Symbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
+    }
+    //Метод для заполнения массива и создания первоначальной игровой доски
     static void BoardFill(char[,] board)
     {
         for (int i = 0; i < board.GetLength(0); i++)
@@ -13,7 +38,7 @@ class ConsoleApp1
             }
         }
     }
-    static void BoardDisplay( ref char[,] board)
+    static void BoardDisplay(ref char[,] board)
     {
         for (int i = 0; i < board.GetLength(0); i++)
         {
@@ -25,10 +50,10 @@ class ConsoleApp1
             Console.WriteLine();
         }
     }
-
-    static void MakeMove(ref char[,] board)
+    // Метод для обработки ходовы
+    static void MakeMove(char[,] board, Player CurrentPlayer)
     {
-        Console.WriteLine("Enter move");
+        Console.WriteLine($"{CurrentPlayer.Name} your turn to make move");
         while (true)
         {
             int row, col;
@@ -47,7 +72,7 @@ class ConsoleApp1
             }
             else
             {
-                board[row, col] = 'X';
+                board[row, col] = CurrentPlayer.Symbol;
                 BoardDisplay(ref board);
                 break;
             }      
@@ -55,40 +80,44 @@ class ConsoleApp1
       
     }
 
-    static void CheckWinner( ref char[,] board, ref Boolean isWinning)
+    static void CheckWinner(char[,] board, ref Boolean isWinning, Player CurrentPlayer)
     {
-       
-        if (board[0, 0] == 'X' && board[0, 1] == 'X' && board[0, 2] == 'X' ||
-            board[1, 0] == 'X' && board[1, 1] == 'X' && board[1, 2] == 'X' ||
-            board[2, 0] == 'X' && board[2, 1] == 'X' && board[2, 2] == 'X' ||
+        char SymbolOfPlayer = CurrentPlayer.Symbol;
+        if (board[0, 0] == SymbolOfPlayer && board[0, 1] == SymbolOfPlayer && board[0, 2] == SymbolOfPlayer ||
+            board[1, 0] == SymbolOfPlayer && board[1, 1] == SymbolOfPlayer && board[1, 2] == SymbolOfPlayer ||
+            board[2, 0] == SymbolOfPlayer && board[2, 1] == SymbolOfPlayer && board[2, 2] == SymbolOfPlayer ||
 
             // Вертикаль
-            board[0, 0] == 'X' && board[1, 0] == 'X' && board[2, 0] == 'X' ||
-            board[0, 1] == 'X' && board[1, 1] == 'X' && board[2, 1] == 'X' ||
-            board[0, 2] == 'X' && board[1, 2] == 'X' && board[2, 2] == 'X' ||
+            board[0, 0] == SymbolOfPlayer && board[1, 0] == SymbolOfPlayer && board[2, 0] == SymbolOfPlayer ||
+            board[0, 1] == SymbolOfPlayer && board[1, 1] == SymbolOfPlayer && board[2, 1] == SymbolOfPlayer ||
+            board[0, 2] == SymbolOfPlayer && board[1, 2] == SymbolOfPlayer && board[2, 2] == SymbolOfPlayer ||
 
             // Диагонали
-            board[0, 0] == 'X' && board[1, 1] == 'X' && board[2, 2] == 'X' ||
-            board[0, 2] == 'X' && board[1, 1] == 'X' && board[2, 0] == 'X')
+            board[0, 0] == SymbolOfPlayer && board[1, 1] == SymbolOfPlayer && board[2, 2] == SymbolOfPlayer ||
+            board[0, 2] == SymbolOfPlayer && board[1, 1] == SymbolOfPlayer && board[2, 0] == SymbolOfPlayer)
         {
             isWinning = true;
-            Console.WriteLine("You won!");
+            Console.WriteLine($"Player {CurrentPlayer.Name} won the game!");
         }
     }  
     
     static void Main(string[] args)
     {
         Boolean isWinning = false;
+        Player player1 = new Player("Nick", 'X');
+        Player player2 = new Player("Sasha", 'O');
         char[,] board = new char[3, 3];
+        Player CurrentPlayer = player1;
         BoardFill(board);
         BoardDisplay(ref board);
         while (isWinning == false)
         {
           
             Console.WriteLine();
-            MakeMove(ref board); 
-            CheckWinner(ref board,ref isWinning);
-            
+            MakeMove(board,CurrentPlayer); 
+            CheckWinner(board,ref isWinning,CurrentPlayer);
+            //Смена сторон
+            CurrentPlayer = (CurrentPlayer == player1) ? player2 : player1;
         }
        
         
